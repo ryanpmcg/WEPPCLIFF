b7eda37f1592499cb4a31ec103ce24ed4e586db4
ryanpmcg <53908244+ryanpmcg@users.noreply.github.com>
2019-12-09 21:10:56 -0500

    Updated Windows AutoTest

--- AUTOTEST/AutoTest.bat
+++ AUTOTEST/AutoTest.bat
@@ -17,10 +17,15 @@ TIMEOUT 10
 "%PathToRscript%" --vanilla WEPPCLIFF.R --args -o AUTOTEST/OUTPUT -e AUTOTEST/OUTPUT -fn AutoTest.7 -f AH537_10MIN.csv -pi 10 -cp f -u m PRECIP -sm 2 -la y -verb t -ei t -ee ARS -ipb t -pd t -ed 1
 "%PathToRscript%" --vanilla WEPPCLIFF.R --args -o AUTOTEST/OUTPUT -e AUTOTEST/OUTPUT -fn AutoTest.8 -f AH537_15MIN.csv -pi 15 -cp f -u m PRECIP -sm 2 -la y -verb t -ei t -ee ARS -ipb t -pd t -ed 1
 
-REM CLS
-REM ECHO Running checksum tests. Almost done... & ECHO.
-REM SET FAIL=0
-REM FOR /L %%ITER IN (1,1,8)
-REM	DO ()
-REM FC C:\GITHUB\WEPPCLIFF\AUTOTEST\BENCHMARKS\AutoTest.1.cli C:\GITHUB\WEPPCLIFF\AUTOTEST\OUTPUT\AutoTest.1.cli
-REM if %FAIL%==0 (ECHO. & ECHO CONGRATULATIONS! Your WEPPCLIFF installation was successfully verified by 8 & ECHO automated test runs and 16 highly unique file outputs. Now you are ready to use & ECHO WEPPCLIFF with peace of mind, knowing everything is working as intended. & ECHO ) else (ECHO OOPS! Something is not right. Your WEPPCLIFF installation failed at least one & ECHO of 8 automated test runs. Please remove and reinstall WEPPCLIFF then try this & ECHO test again. If the problem persists and you believe the problem lies within & ECHO WEPPCLIFF, please create an issue at: https://github.com/ryanpmcg/WEPPCLIFF & ECHO  & ECHO Thank you! Hopefully the WEPPCLIFF community can help you get up and running & ECHO soon. & ECHO.)
+CLS
+ECHO Running comparison tests. Almost done... & ECHO.
+SET FAIL=0
+SETLOCAL EnableDelayedExpansion
+
+FOR %%I IN (1 2 3 4 5 6 7 8) DO (
+	FC AUTOTEST\BENCHMARKS\AutoTest.%%I.cli AUTOTEST\OUTPUT\AutoTest.%%I.cli >NUL
+	IF !ERRORLEVEL! NEQ 0 (SET FAIL=1) ELSE (ECHO Passed CLI Test %%I)
+	)
+
+IF %FAIL%==0 (ECHO. & ECHO CONGRATULATIONS! Your WEPPCLIFF installation was successfully verified by 8 & ECHO automated test runs and 16 highly unique file outputs. Now you are ready to use & ECHO WEPPCLIFF with peace of mind, knowing everything is working as intended. & ECHO ) ELSE (ECHO. & ECHO OOPS! Something is not right. Your WEPPCLIFF installation failed at least one & ECHO of 8 automated test runs. Please remove and reinstall WEPPCLIFF then try this & ECHO test again. If the problem persists and you believe the problem lies within & ECHO WEPPCLIFF, please create an issue at: https://github.com/ryanpmcg/WEPPCLIFF & ECHO  & ECHO Thank you! Hopefully the WEPPCLIFF community can help you get up and running & ECHO soon. & ECHO.)
+TIMEOUT 60
