a699b3c5f639c9e6fe2392f365db0ceb68b6cd80
Ryan McGehee <53908244+ryanpmcg@users.noreply.github.com>
2024-01-17 12:07:14 -0600

    Update Install.bat

--- INSTALLER/Install.bat
+++ INSTALLER/Install.bat
@@ -6,6 +6,8 @@ DIR "C:\Program Files\R" /s /b | FIND "bin\Rscript.exe"
 @ECHO.
 @ECHO Please copy one of the paths above to save in your system path.
 @ECHO If no matching file was found, you will need to install R first.
+@ECHO This script assumes the default location and does not look elsewhere.
+@ECHO If you installed elsewhere, you are responsible for knowing where.
 @ECHO.
 @ECHO off
 SET /p PathToRscript=Enter the path to Rscript here:
@@ -13,6 +15,7 @@ SET /p PathToRscript=Enter the path to Rscript here:
 @ECHO.
 @ECHO.
 @ECHO You selected %PathToRscript%
+SETX PATH "%PATH%;%PathToRscript%"
 @ECHO.
 @ECHO.
 DIR ".." /s /b | FIND "WEPPCLIFF.R" > TMP.txt
